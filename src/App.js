45d36d323d652a283b60dc06066405f7bf5e152d
Grace Omole <olayinkagrace112.ob@gmail.com>
2023-05-11 15:44:20 +0100

    another 3d-render

--- src/App.js
+++ src/App.js
@@ -1,43 +1,62 @@
-import React, { Suspense } from "react";
-import { Canvas } from "react-three-fiber";
+// import React from "react";
+// import { Canvas, useThree } from "@react-three/fiber";
+// import { OrbitControls } from "@react-three/drei";
 
-import "./App.css";
+// import "./App.css";
+// // import { Model } from "./Scene";
+// // import { Model } from "./Braid";
+// // import { Model } from "./Scene-Hair";
+// // import { Model } from "./Hair3";
+// import { Model } from "./Hair3";
 
-import { useGLTF } from "@react-three/drei";
+
+
+
+// function App() {
+//   return (
+//     <>
+//       <Canvas>
+//         <ambientLight />
+//         <pointLight position={[5, 5, 5]} intensity={1} />
+//         <pointLight position={[-3, -3, 2]} />
+        
+
+//         <Model />
+
+//        <OrbitControls />
+//       </Canvas>
+      
+//       ;
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, Stage, OrbitControls, Float } from "@react-three/drei";
 
 function Model(props) {
-  const { nodes, materials } = useGLTF("/Hair3.gltf");
-  return (
-    <group {...props} dispose={null}>
-      <mesh
-        geometry={nodes.Hair008.geometry}
-        material={materials["Hair Mat"]}
-      />
-    </group>
-  );
+  const { scene } = useGLTF("/Hair.glb");
+  return <primitive object={scene} {...props} />
 }
-
-useGLTF.preload("/Hair3.gltf");
 
 function App() {
   return (
-    <>
-      <Canvas style={{ background: "#000", width:"auto" }}>
-        <ambientLight intensity={1} />
-        <spotLight
-          intensity={0.5}
-          angle={0.1}
-          penumbra={1}
-          position={[10, 15, 10]}
-          castShadow
-        />
-        <Suspense fallback={null}>
-          <Model />
-        </Suspense>
-      </Canvas>
-      ;
-    </>
+    <Canvas dpr={[1,2]}  camera={{ fov: 45 }} style={{"position": "absolute"}}>
+      {/* <color attach="background" args={["#101010"]} /> */}
+      {/* <PresentationControls speed={1.5} global zoom={.5} polar={[-0.1, Math.PI / 4]}> */}
+        <Stage environment={"sunset"}>
+     
+   
+          <Model scale={0.01} />
+      
+        </Stage>
+      <OrbitControls />
+    </Canvas>
   );
 }
 
 export default App;
+
